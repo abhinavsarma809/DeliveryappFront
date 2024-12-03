b27e03ef9defc372fd55213a1eae3ef6322d91f8
Bhamidipati Sarma <abhinavsarma245@gmail.com>
2024-12-03 17:08:48 +0530

    changing

--- pages/services/index.js
+++ pages/services/index.js
@@ -2,7 +2,7 @@
 
 
 
-const BACKEND_URL = 'http://localhost:5000'; 
+const BACKEND_URL = 'https://backend-one-virid-81.vercel.app'; 
 
 export const login = async (data) => {
     console.log(BACKEND_URL); 
